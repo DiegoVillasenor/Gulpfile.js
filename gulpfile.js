036873395092ec803b76e5657fb916be5aaa49e6
Diego <diego.vid.eco@gmail.com>
2016-03-14 22:36:48 -0600

    adds working prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,49 @@
-var JSPATH = '../themes/edaa-theme/js/',
-	  MAINCSS = '../themes/edaa-theme/style.css';
+// var JSPATH = '../themes/edaa-theme/js/',
+// 	  MAINCSS = '../themes/edaa-theme/style.css';
 
 var gulp = require('gulp');
-// var concat = require('gulp-concat');
-// var browserSync = require('browser-sync');
 
-	//watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps'); //sass sourcemaps
+var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 
-//gulp.task('default', ['scripts']);
-gulp.task('default', function() { console.log(JSPATH)});
-
- 
-gulp.task('scripts', function() {
-  gulp.src([JSPATH+'swiper.jquery.min.js', JSPATH+'functions.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./themes/edaa-theme/js/'))
-    .pipe(browserSync.reload({ stream: true}))
+gulp.task('hello', function() {
+  console.log(gulp.src);
 });
+
+gulp.task('sass', function(){
+  return gulp.src('test/scss/style.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dest'))
+    // .pipe(browserSync.reload({ stream: true }))
+});
+
+
+/**
+ * To copy files from one place to another.
+ *
+ * @src http://ilikekillnerds.com/2014/07/copying-files-from-one-folder-to-another-in-gulp-js/
+ * @param  {[type]} ) {              gulp.src('./bower_components/font-awesome/fonts*.{ttf,woff,eof,svg}')   .pipe(gulp.dest('./fonts'));} [description]
+ * @return {[type]}   [description]
+ */
+gulp.task('copyfonts', function() {
+   gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
+   .pipe(gulp.dest('./fonts'));
+});
+ 
+// gulp.task('scripts', function() {
+//   gulp.src([JSPATH+'swiper.jquery.min.js', JSPATH+'functions.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(gulp.dest('./themes/edaa-theme/js/'))
+//     .pipe(browserSync.reload({ stream: true}))
+// });
 
 // gulp.task('stream', function () {
 //     return gulp.src(MAINCSS)
@@ -31,15 +58,18 @@ gulp.task('scripts', function() {
 //             .on('end', cb);
 //     });
 // });
-// 
-// gulp.task('watch', ['browserSync', 'scripts'], function() {
-//   //gulp.watch(MAINCSS, ['sass']); 
-//   gulp.watch(JSPATH+'*.js', ['scripts']); 
 
-// });
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+  gulp.watch('test/scss/**/*.scss', ['sass']); 
+  // gulp.watch('*.js', ['scripts']); 
 
-// gulp.task('browserSync', function() {
-//   browserSync({
-//     proxy: 'localhost:8888'
-//   });
-// });
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(['dest/*.css'],{ //files to inject
+     host: "localhost",
+            port: 8888
+  });
+});
+
+gulp.task('default', ['browser-sync']);
