7480474775d62efd31a8491aaa3ca783684cc095
Diego <diego.vid.eco@gmail.com>
2016-07-23 22:07:25 -0500

    agrega babel, y funciona para compilar scripts, pero exports no funciona en el buscador, es necesario agregar webpack

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@
  */
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var babel = require("gulp-babel");
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps'); //sass sourcemaps
@@ -70,12 +71,14 @@ gulp.task('copyfonts', function() {
    .pipe(gulp.dest('./fonts'));
 });
  
-// gulp.task('scripts', function() {
-//   gulp.src([JSPATH+'swiper.jquery.min.js', JSPATH+'functions.js'])
-//     .pipe(concat('all.js'))
-//     .pipe(gulp.dest('./themes/edaa-theme/js/'))
-//     .pipe(browserSync.reload({ stream: true}))
-// });
+gulp.task("scripts", function () {
+  return gulp.src("test/js/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("../sourcemaps/scss/"))
+    .pipe(gulp.dest("dest"));
+});
 
 // gulp.task('stream', function () {
 //     return gulp.src(MAINCSS)
@@ -91,10 +94,10 @@ gulp.task('copyfonts', function() {
 //     });
 // });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function() {
   gulp.watch('test/scss/**/*.scss', ['sass']); 
+  gulp.watch('test/js/**/*.js', ['scripts']); 
   // browserSync.reload("**/*.css");
-  // gulp.watch('*.js', ['scripts']); 
 
 });
 
---
+++ test/js/src/script.js
@@ -0,0 +1,6 @@
+export var hola = function(s) {
+	var x = () => this.sexy = s;
+	console.log(s);
+}
+
+hola('Diego');
